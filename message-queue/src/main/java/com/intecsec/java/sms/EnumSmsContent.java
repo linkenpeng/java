921a1f77ef4e7c669dca875a1c005f1f19668935
Peter.Peng <peter.peng@watsons.com.cn>
2020-09-28 17:23:48 +0800

    新增腾讯发短信功能

---
+++ message-queue/src/main/java/com/intecsec/java/sms/EnumSmsContent.java
@@ -0,0 +1,28 @@
+package com.intecsec.java.sms;
+
+/**
+ * @author peter.peng
+ * @date 2020/9/28
+ */
+public enum EnumSmsContent {
+	REG("1", ""),
+	CHECK_PASS("2", ""),
+	CHECK_UN_PASS("3", ""),
+	;
+
+	private String templateId;
+	private String templateContent;
+
+	EnumSmsContent(String templateId, String templateContent) {
+		this.templateId = templateId;
+		this.templateContent = templateContent;
+	}
+
+	public String getTemplateId() {
+		return templateId;
+	}
+
+	public String getTemplateContent() {
+		return templateContent;
+	}
+}

9a3f31bf95ac675838ec91eece2d735a75ca7e9f
Peter.Peng <peter.peng@watsons.com.cn>
2021-03-02 11:21:34 +0800

    add kafka

---
+++ message-queue/src/main/java/com/intecsec/java/mq/MessageQueueApp.java
@@ -0,0 +1,17 @@
+package com.intecsec.java.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author peter.peng
+ * @date 2020/7/27
+ */
+@SpringBootApplication
+public class MessageQueueApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageQueueApp.class, args);
+	}
+
+}

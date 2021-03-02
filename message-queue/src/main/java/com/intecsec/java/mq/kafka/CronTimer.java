a4d97d44aedb038c09289d5714776b64bc8d74df
Peter.Peng <peter.peng@watsons.com.cn>
2021-03-02 18:49:16 +0800

    add kafka

---
+++ message-queue/src/main/java/com/intecsec/java/mq/kafka/CronTimer.java
@@ -0,0 +1,55 @@
+package com.intecsec.java.mq.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Peter.Peng
+ * @date 2021/3/2
+ */
+@EnableScheduling
+@Component
+public class CronTimer {
+
+	@Autowired
+	private KafkaListenerEndpointRegistry registry;
+
+	@Autowired
+	private ConsumerFactory consumerFactory;
+
+	public ConcurrentKafkaListenerContainerFactory delayContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory container = new ConcurrentKafkaListenerContainerFactory();
+		container.setConsumerFactory(consumerFactory);
+		//禁止KafkaListener自启动
+		container.setAutoStartup(false);
+		return container;
+	}
+
+	// 监听器
+	@KafkaListener(id="timingConsumer",topics = "topic1",containerFactory = "delayContainerFactory")
+	public void onMessage1(ConsumerRecord<?, ?> record){
+		System.out.println("消费成功："+record.topic()+"-"+record.partition()+"-"+record.value());
+	}
+​
+	// 定时启动监听器
+	@Scheduled(cron = "0 42 11 * * ? ")
+	public void startListener() {
+		System.out.println("启动监听器...");
+		if (!registry.getListenerContainer("timingConsumer").isRunning()) {
+			registry.getListenerContainer("timingConsumer").start();
+		}
+	}
+
+	@Scheduled(cron = "0 45 11 * * ? ")
+	public void shutDownListener() {
+		System.out.println("关闭监听器...");
+		registry.getListenerContainer("timingConsumer").pause();
+	}
+}
---
+++ message-queue/src/main/java/com/intecsec/java/mq/kafka/CustomerPartitioner.java
@@ -0,0 +1,28 @@
+package com.intecsec.java.mq.kafka;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+import java.util.Map;
+
+/**
+ * @author Peter.Peng
+ * @date 2021/3/2
+ */
+public class CustomerPartitioner implements Partitioner {
+
+	@Override
+	public int partition(String s, Object o, byte[] bytes, Object o1, byte[] bytes1, Cluster cluster) {
+		return 0;
+	}
+
+	@Override
+	public void close() {
+
+	}
+
+	@Override
+	public void configure(Map<String, ?> map) {
+
+	}
+}

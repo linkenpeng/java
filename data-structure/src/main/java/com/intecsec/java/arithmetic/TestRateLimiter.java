9f0ba09deb079097cef780d029d34ada1cfdb0d7
peter <peter.peng@watsons.com.cn>
2021-03-06 11:58:18 +0800

    redis cluster pipline

---
+++ data-structure/src/main/java/com/intecsec/java/arithmetic/TestRateLimiter.java
@@ -0,0 +1,28 @@
+package com.intecsec.java.arithmetic;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @description: 令牌桶限流算法
+ * @author: peter.peng
+ * @create: 2021-03-06 11:55
+ **/
+public class TestRateLimiter {
+
+    public static void main(String[] args) {
+        final RateLimiter rateLimiter = RateLimiter.create(5);
+
+        for (int i = 0; i < 10; i++) {
+            long timeOut = (long) 0.5;
+            boolean isValid = rateLimiter.tryAcquire(timeOut, TimeUnit.SECONDS);
+            System.out.println("任务" + i + "执行是否有效:" + isValid);
+            if (!isValid) {
+                continue;
+            }
+            System.out.println("任务" + i + "在执行");
+        }
+        System.out.println("结束");
+    }
+}

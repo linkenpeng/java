3a183aa6c33832da9db3a51d9d814046d5cd2e63
peter <peter.peng@watsons.com.cn>
2021-03-10 17:56:33 +0800

    search

---
+++ search/src/main/java/com/intecsec/java/search/split/JcseqTest.java
@@ -0,0 +1,15 @@
+package com.intecsec.java.search.split;
+
+/**
+ * @description:
+ *
+ * https://github.com/Lihuanghe/jcseg
+ *
+ * @author: peter.peng
+ * @create: 2021-03-10 17:27
+ **/
+public class JcseqTest {
+    public static void main(String[] args) {
+
+    }
+}

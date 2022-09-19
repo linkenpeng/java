21960a6a64a9217428e51f6acb65f1bba1c9483c
Peter.Peng <peter.peng@watsons.com.cn>
2022-09-19 18:31:06 +0800

    AliyunSts

--- java-basic/src/main/java/com/intecsec/java/basic/third/AliyunSts.java
+++ java-basic/src/main/java/com/intecsec/java/basic/third/AliyunSts.java
@@ -3,6 +3,11 @@ package com.intecsec.java.basic.third;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
+import com.aliyun.oss.internal.OSSHeaders;
+import com.aliyun.oss.model.CannedAccessControlList;
+import com.aliyun.oss.model.ObjectMetadata;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.StorageClass;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
@@ -12,6 +17,7 @@ import com.aliyuncs.sts.model.v20150401.AssumeRoleRequest;
 import com.aliyuncs.sts.model.v20150401.AssumeRoleResponse;
 import com.google.gson.Gson;
 
+import java.io.File;
 import java.net.URL;
 import java.util.Date;
 
@@ -38,11 +44,13 @@ import java.util.Date;
  */
 public class AliyunSts {
 
-    private static final String endpoint = "oss-cn-shanghai.aliyuncs.com";
+    private static final String endpoint = "";
+    private static final String ossEndpoint = "https://oss-cn-shanghai.aliyuncs.com";
     private static final String regionId = "cn-shanghai";
     private static final String accessKeyId = "";
-    private static final String secret = "";
+    private static final String accessKeySecret = "";
     private static final String bucketName = "";
+    private static final String  objectName = "";
     private static final String roleArn = "";
     private static final String policy = "{" +
             "    \"Version\": \"1\", " +
@@ -58,7 +66,50 @@ public class AliyunSts {
             "}";
 
     public static void main(String[] args) throws Throwable {
-        getSTSURL();
+        uploadFile();
+        //getSTSURL();
+    }
+
+    public static void uploadFile() throws Exception {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        // String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        // String accessKeyId = "yourAccessKeyId";
+        // String accessKeySecret = "yourAccessKeySecret";
+        // 填写Bucket名称，例如examplebucket。
+        //String bucketName = "examplebucket";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        // String objectName = "exampledir/exampleobject.txt";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
+        String filePath= "E:\\my\\identityCard.jpg";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(ossEndpoint, accessKeyId, accessKeySecret);
+
+        try {
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new File(filePath));
+            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setHeader(OSSHeaders.OSS_SERVER_SIDE_ENCRYPTION, "AES256");
+            metadata.setObjectAcl(CannedAccessControlList.Private);
+            putObjectRequest.setMetadata(metadata);
+
+            // 上传文件。
+            ossClient.putObject(putObjectRequest);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
     }
 
     public static URL getSTSURL() {
@@ -71,7 +122,7 @@ public class AliyunSts {
         // 从STS服务获取的安全令牌（SecurityToken）。
         String securityToken = response.getCredentials().getSecurityToken();
         // 填写Object完整路径，例如exampleobject.txt。Object完整路径中不能包含Bucket名称。
-        String objectName = "";
+        //String objectName = "idcard/identityCard.jpg";
 
         // 从STS服务获取临时访问凭证后，您可以通过临时访问密钥和安全令牌生成OSSClient。
         // 创建OSSClient实例。
@@ -99,7 +150,7 @@ public class AliyunSts {
     public static AssumeRoleResponse getRole() {
         //构建一个阿里云客户端，用于发起请求。
         //设置调用者（RAM用户或RAM角色）的AccessKey ID和AccessKey Secret。
-        DefaultProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, secret);
+        DefaultProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, accessKeySecret);
         IAcsClient client = new DefaultAcsClient(profile);
 
         //构造请求，设置参数。

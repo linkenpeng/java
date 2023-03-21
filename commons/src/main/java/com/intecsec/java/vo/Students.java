14dc368f1309085c1730f9e36b14ad1d5ec01bc9
Peter.Peng <peter.peng@watsons.com.cn>
2023-03-21 11:06:52 +0800

    add webservice test

--- commons/src/main/java/com/intecsec/java/vo/Students.java
+++ commons/src/main/java/com/intecsec/java/vo/Students.java
@@ -3,9 +3,11 @@ package com.intecsec.java.vo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
-public class Students {
+public class Students implements Serializable {
 	private int id;
 	private String name;
 	private int age;

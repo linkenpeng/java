476b4a5cfca4f62bdd9c06fb0e3c77a8dad3f039
linken <collin_linken@qq.com>
2023-05-07 22:40:39 +0800

    add map,set,list test

---
+++ java-basic/src/main/java/com/intecsec/java/basic/collection/ListTest.java
@@ -0,0 +1,128 @@
+package com.intecsec.java.basic.collection;
+
+import com.intecsec.java.basic.keyword.Utils;
+
+import java.text.DecimalFormat;
+import java.util.*;
+
+/**
+ * @description:
+ * @author: peter.peng
+ * @create: 2023-05-07 09:10
+ **/
+public class ListTest {
+    public static final int NUM = 100000;
+
+    public static void main(String[] args) {
+        list();
+        linkedList();
+        vector();
+    }
+
+    public static void list() {
+        System.out.println("ArrayList================================");
+        List<Integer> arrList = new ArrayList();
+
+        for(int i = 0; i < 10; i++) {
+            arrList.add(i);
+        }
+        System.out.println(arrList);
+        System.out.println(arrList.get(3));
+        arrList.remove(3);
+        System.out.println(arrList);
+        arrList.add(3, 19);
+        System.out.println(arrList);
+
+        long startTime = System.nanoTime();
+        for(int i = 0; i < NUM; i++) {
+            arrList.add(0, i);
+        }
+        Utils.duration(startTime);
+
+        traverseByIterator(arrList);
+        traverseByIndex(arrList);
+        traverseByFor(arrList);
+    }
+
+    public static void linkedList() {
+        System.out.println("LinkedList================================");
+        List<Integer> arrList = new LinkedList();
+
+        for(int i = 0; i < 10; i++) {
+            arrList.add(i);
+        }
+        System.out.println(arrList);
+        System.out.println(arrList.get(3));
+        arrList.remove(3);
+        System.out.println(arrList);
+        arrList.add(3, 19);
+        System.out.println(arrList);
+
+        long startTime = System.nanoTime();
+        for(int i = 0; i < NUM; i++) {
+            arrList.add(0, i);
+        }
+        Utils.duration(startTime);
+
+        traverseByIterator(arrList);
+        traverseByIndex(arrList);
+        traverseByFor(arrList);
+    }
+
+    public static void vector() {
+        System.out.println("Vector================================");
+        Vector arrList = new Vector();
+
+        for(int i = 0; i < 10; i++) {
+            arrList.add(i);
+        }
+        System.out.println(arrList);
+        System.out.println(arrList.get(3));
+        arrList.remove(3);
+        System.out.println(arrList);
+        arrList.add(3, 19);
+        System.out.println(arrList);
+
+        long startTime = System.nanoTime();
+        for(int i = 0; i < NUM; i++) {
+            arrList.add(i);
+        }
+        Utils.duration(startTime);
+
+        traverseByIterator(arrList);
+        traverseByIndex(arrList);
+        traverseByFor(arrList);
+    }
+
+    public static void traverseByIterator(List list) {
+        long startTime = System.nanoTime();
+        System.out.println("=======迭代器遍历=======");
+        Iterator iterator = list.iterator();
+        while(iterator.hasNext()) {
+            iterator.next();
+        }
+        Utils.duration(startTime);
+    }
+
+
+
+    public static void traverseByIndex(List list) {
+        long startTime = System.nanoTime();
+        System.out.println("=======随机索引遍历=======");
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i);
+        }
+        Utils.duration(startTime);
+    }
+
+    public static void traverseByFor(List<Integer> list) {
+        long startTime = System.nanoTime();
+        System.out.println("=======for遍历=======");
+        for(Integer i : list) {
+            ;
+        }
+        Utils.duration(startTime);
+    }
+
+
+}
--- java-basic/src/main/java/com/intecsec/java/basic/collection/SetTest.java
+++ java-basic/src/main/java/com/intecsec/java/basic/collection/SetTest.java
@@ -1,7 +1,6 @@
 package com.intecsec.java.basic.collection;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @description:
@@ -11,6 +10,18 @@ import java.util.Set;
 public class SetTest {
 
     public static void main(String[] args) {
+
+        hashset();
+        linkedHashSet();
+        treeSet();
+
+
+    }
+
+
+    public static void  hashset() {
+        System.out.println("HashSet =================================");
+
         Set<Integer> set = new HashSet<>();
         set.add(1);
         set.add(2);
@@ -24,5 +35,52 @@ public class SetTest {
         System.out.println(sets.contains("1"));
         sets.add("1");
         System.out.println(sets);
+
+        initData(set);
+        traverseForIterator(set);
+        traverseForFor(set);
+    }
+
+    public static void traverseForIterator(Set<Integer> set) {
+        System.out.println("traverseForIterator============");
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static void traverseForFor(Set<Integer> set) {
+        System.out.println("traverseForFor============");
+        for(Integer i : set) {
+            System.out.println(i);
+        }
+    }
+
+    public static void initData(Set<Integer> set) {
+        // HashSet LinkedHashSet 会调用 hashcode equals
+        // TreeSet 会调用 compareTo
+        set.add(1);
+        set.add(3);
+        set.add(10);
+        set.add(6);
+        set.add(40);
+        set.add(30);
+    }
+
+    public static void  linkedHashSet() {
+        System.out.println("LinkedHashSet =================================");
+        Set<Integer> set = new LinkedHashSet();
+        initData(set);
+        traverseForIterator(set);
+        traverseForFor(set);
+    }
+
+    public static void  treeSet() {
+        System.out.println("TreeSet =================================");
+        Set<Integer> set = new TreeSet();
+        initData(set);
+        traverseForIterator(set);
+        traverseForFor(set);
+
     }
 }

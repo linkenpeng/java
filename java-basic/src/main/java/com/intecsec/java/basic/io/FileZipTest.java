ad5883d15eb8c1df794e49c97f672fd6b1b1117a
linken <collin_linken@qq.com>
2023-05-11 22:15:20 +0800

    add zip test

--- java-basic/src/main/java/com/intecsec/java/basic/io/FileZipTest.java
+++ java-basic/src/main/java/com/intecsec/java/basic/io/FileZipTest.java
@@ -2,6 +2,7 @@ package com.intecsec.java.basic.io;
 
 import java.io.*;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
@@ -14,10 +15,45 @@ public class FileZipTest {
     public static void main(String[] args) {
         String file = "/tmp/test.txt";
         String zipFile = "/tmp/test.zip";
-        zipCompress(file, zipFile);
+        // zip(file, zipFile);
+        // unZip(zipFile);
+
+        String fileDirectory = "/tmp/test";
+        String mulZipFile = "/tmp/test_mul.zip";
+        // multiZip(fileDirectory, mulZipFile);
+
+        unZipMul(mulZipFile);
     }
 
-    public static void zipCompress(String filePath, String zipFilePath) {
+    public static void multiZip(String filePath, String zipFilePath) {
+        try {
+            ZipOutputStream zipOutput = new ZipOutputStream(new FileOutputStream(zipFilePath));
+            zipOutput.setComment("single file zip");
+
+            int temp = 0;
+            InputStream inputStream = null;
+            File file = new File(filePath);
+            if(file.isDirectory()) {
+                File[] files = file.listFiles();
+                for (int i = 0; i < files.length; i++) {
+                    File filez = files[i];
+                    inputStream = new FileInputStream(filez);
+                    zipOutput.putNextEntry(new ZipEntry(file.getName() + File.pathSeparator
+                    + filez.getName()));
+                    System.out.println("正在压缩文件" + filez.getName());
+                    while ((temp = inputStream.read()) != -1) {
+                        zipOutput.write(temp);
+                    }
+                    inputStream.close();
+                }
+            }
+            zipOutput.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void zip(String filePath, String zipFilePath) {
         try {
             File file = new File(filePath);
             File zipFile = new File(zipFilePath);
@@ -38,8 +74,74 @@ public class FileZipTest {
         }
     }
 
-    public static void zipUnCompress(String zipFile) {
+    public static void unZip(String zipFile) {
+        try {
+            File zip = new File(zipFile);
+            ZipInputStream zipInputStream = null;
+            zipInputStream = new ZipInputStream(new FileInputStream(zip));
+            ZipEntry nextEntry = zipInputStream.getNextEntry();
+            System.out.println("压缩实体名称" + nextEntry.getName());
 
+            File outFile = new File("/tmp/" + nextEntry.getName());
+            OutputStream outputStream = new FileOutputStream(outFile);
+            int temp = 0;
+            while ((temp = zipInputStream.read()) != -1) {
+                outputStream.write(temp);
+            }
+
+            zipInputStream.close();
+            outputStream.close();
+
+            System.out.println("unzip done");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void unZipMul(String zipFilePath) {
+        try {
+            File zip = new File(zipFilePath);
+            File outFile = null;
+            ZipFile zipFile = new ZipFile(zip);
+            ZipInputStream zipInputStream = null;
+            OutputStream out = null;
+            InputStream input = null;
+            ZipEntry entry = null;
+            zipInputStream = new ZipInputStream(new FileInputStream(zip));
+
+            while ((entry = zipInputStream.getNextEntry()) != null) {
+                System.out.println("解压缩" + entry.getName());
+                outFile = new File("/tmp/" + entry.getName());
+                if(!outFile.getParentFile().exists()) {
+                    outFile.getParentFile().mkdirs();
+                }
+                if(!outFile.exists()) {
+                    if(entry.isDirectory()) {
+                        outFile.mkdirs();
+                        System.out.println("create directory");
+                    } else {
+                        outFile.createNewFile();
+                        System.out.println("create new file");
+                    }
+                }
+                if(!entry.isDirectory()) {
+                    input = zipFile.getInputStream(entry);
+                    out = new FileOutputStream(outFile);
+                    int temp = 0;
+                    while ((temp = input.read()) != -1) {
+                        out.write(temp);
+                    }
+                    input.close();
+                    out.close();
+                }
+            }
+
+            zipInputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }

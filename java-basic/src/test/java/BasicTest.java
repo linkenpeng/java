4c0261888977a7ca67a93e634036d7da5a461476
linken <collin_linken@qq.com>
2023-05-16 08:08:21 +0800

    io test

---
+++ java-basic/src/test/java/BasicTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @description:
+ * @author: peter.peng
+ * @create: 2023-05-15 07:16
+ **/
+public class BasicTest {
+
+    @Test
+    public void testBasic() {
+        assertEquals(true, 1 > -1);
+    }
+}

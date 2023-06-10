5cdad85dc9f5a507e08bff30acb3f1c06a5cb1a0
linkenpeng <collin_linken@qq.com>
2023-06-10 10:05:42 +0800

    lambda

---
+++ java-basic/src/main/java/com/intecsec/java/basic/lambda/app/LambdaObject.java
@@ -0,0 +1,23 @@
+package com.intecsec.java.basic.lambda.app;
+
+public class LambdaObject {
+
+	public static void main(String[] args) {
+
+		IntOperation iop = x -> x * 2;
+		DoubleOperation dop = x -> x * 2;
+		
+		Object obj1 = iop;
+		Object obj2 = dop;
+
+	}
+
+}
+
+interface IntOperation {
+	int operate(int i);
+}
+
+interface DoubleOperation {
+	double operate(double i);
+}

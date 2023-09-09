ee40adfbb7a272f7ea096c56c5d76c143646a488
linkenpeng <collin_linken@qq.com>
2023-09-09 11:28:49 +0800

    cache test

--- cache/src/main/java/com/intecsec/java/cache/CacheApp.java
+++ cache/src/main/java/com/intecsec/java/cache/CacheApp.java
@@ -3,7 +3,6 @@ package com.intecsec.java.cache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author peter.peng

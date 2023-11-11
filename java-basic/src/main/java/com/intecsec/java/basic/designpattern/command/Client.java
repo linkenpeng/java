9069b8ccf78e265fab6083a183785d6de69f1421
linkenpeng <collin_linken@qq.com>
2023-11-11 11:27:49 +0800

    add command undo

--- java-basic/src/main/java/com/intecsec/java/basic/designpattern/command/Client.java
+++ java-basic/src/main/java/com/intecsec/java/basic/designpattern/command/Client.java
@@ -1,9 +1,6 @@
 package com.intecsec.java.basic.designpattern.command;
 
-import com.intecsec.java.basic.designpattern.command.cmdimp.LightOffCommand;
-import com.intecsec.java.basic.designpattern.command.cmdimp.LightOnCommand;
-import com.intecsec.java.basic.designpattern.command.cmdimp.TVOffCommand;
-import com.intecsec.java.basic.designpattern.command.cmdimp.TVOnCommand;
+import com.intecsec.java.basic.designpattern.command.cmdimp.*;
 import com.intecsec.java.basic.designpattern.command.receiver.Light;
 import com.intecsec.java.basic.designpattern.command.receiver.TV;
 
@@ -14,6 +11,10 @@ import com.intecsec.java.basic.designpattern.command.receiver.TV;
  **/
 public class Client {
     public static void main(String[] args) {
+        mutil();
+    }
+
+    public static void onOff() {
         Light light = new Light();
         LightOnCommand lightOnCommand = new LightOnCommand(light);
         LightOffCommand lightOffCommand = new LightOffCommand(light);
@@ -29,5 +30,33 @@ public class Client {
 
         invoker.off(2);
         invoker.off(1);
+    }
+
+    public static void undo() {
+        Light light = new Light();
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+        LightOffCommand lightOffCommand = new LightOffCommand(light);
+        Invoker invoker = new Invoker();
+        invoker.setCommand(1, lightOnCommand, lightOffCommand);
+        invoker.on(1);
+        invoker.undo();
+
+        TV tv = new TV();
+        TVOnCommand tvOnCommand = new TVOnCommand(tv);
+        TVOffCommand tvOffCommand = new TVOffCommand(tv);
+        invoker.setCommand(2, tvOnCommand, tvOffCommand);
+        invoker.on(2);
+        invoker.undo();
+    }
+
+    public static void mutil() {
+        Light light = new Light();
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+        TV tv = new TV();
+        TVOnCommand tvOnCommand = new TVOnCommand(tv);
+        Command[] commandsOn = {lightOnCommand, tvOnCommand};
+        MultiCommand multiCommand = new MultiCommand(commandsOn);
+        multiCommand.execute();
+        multiCommand.undo();
     }
 }
--- java-basic/src/main/java/com/intecsec/java/basic/designpattern/command/cmdimp/NoCommand.java
+++ java-basic/src/main/java/com/intecsec/java/basic/designpattern/command/cmdimp/NoCommand.java
@@ -12,4 +12,9 @@ public class NoCommand implements Command {
     public void execute() {
 
     }
+
+    @Override
+    public void undo() {
+
+    }
 }
